2c2aad43b2e1abd472e6c466246fcf569c71c8fb
YieBonface <bonface@yieldexchange.ca>
2024-04-14 16:19:05 +0300

    remove warning by adding const key before layout widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import "package:flutter/material.dart";
 void main() {
   //if you fail to give textdirection on Text widget then the app will crash
   //Please note  note that layout widget like center must have child.
-  runApp(Center(
+  
+  runApp(const Center(
     child: Text(
       " Hello Bonae",
       textDirection: TextDirection.ltr,
