8332d4024c72234153d1150f7dbd4b46601c72b0
YieBonface <bonface@yieldexchange.ca>
2024-04-14 22:48:43 +0300

    How to call functions within a widget in flutter

--- lib/screens/Dashboard.dart
+++ lib/screens/Dashboard.dart
@@ -1,3 +1,5 @@
+import "dart:math";
+
 import "package:flutter/material.dart";
 
 class Dashboard extends StatelessWidget {
@@ -14,24 +16,11 @@ class Dashboard extends StatelessWidget {
         backgroundColor: Colors.red.shade900,
         centerTitle: true,
       ),
-      body: Center(
-        child: Text.rich(
-          TextSpan(
-            text: "Coding ",
-            style: TextStyle(fontSize: 10.0, color: Colors.amber.shade900),
-            children: [
-              TextSpan(
-                text: 'With',
-                style: TextStyle(fontSize: 40.0, color: Colors.red.shade900),
-              ),
-              TextSpan(
-                text: 'Bonae',
-                style: TextStyle(fontSize: 70.0, color: Colors.red.shade900),
-              ),
-            ],
-          ),
-        ),
-      ),
+      body: Center(child: Text("The rando number is ${getRandonInt()}")),
     );
   }
+}
+
+int getRandonInt() {
+  return Random().nextInt(100);
 }
