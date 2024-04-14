14bdfd98a86478bfe7ebe7709e026de3255ba283
YieBonface <bonface@yieldexchange.ca>
2024-04-14 16:10:04 +0300

    Using the center layout widget to position text widget at center of the screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import "package:flutter/material.dart";
 
 void main() {
   //if you fail to give textdirection on Text widget then the app will crash
-  runApp(Text(
-    " Hello Bonae",
-    textDirection: TextDirection.ltr,
+  //Please note  note that layout widget like center must have child.
+  runApp(Center(
+    child: Text(
+      " Hello Bonae",
+      textDirection: TextDirection.ltr,
+    ),
   ));
 }
